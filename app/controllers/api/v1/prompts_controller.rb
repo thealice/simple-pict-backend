eb022503a219b9f71a29e0d9bb231c89b08c087a
Alice Freda <alicefreda@gmail.com>
2020-06-02 18:39:52 -0700

    fix typo in prompts controller

--- app/controllers/api/v1/prompts_controller.rb
+++ app/controllers/api/v1/prompts_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::PromptsController < ApplicationController
         if prompt.save
             render json: prompt, status: :accepted
         else 
-            render json: {errors: prompt.errors.fullmesasges}, status: :unprocessable_entity
+            render json: {errors: prompt.errors.fullmessages}, status: :unprocessable_entity
         end
     end
 
