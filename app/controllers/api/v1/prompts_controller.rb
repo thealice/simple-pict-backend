dcdc5d1c25193bb52d9b4fbfa1ed35f6e3ed85c9
Alice Freda <alicefreda@gmail.com>
2020-06-02 18:32:51 -0700

    add index and create actions to prompts controller

--- app/controllers/api/v1/prompts_controller.rb
+++ app/controllers/api/v1/prompts_controller.rb
@@ -1,10 +1,23 @@
 class Api::V1::PromptsController < ApplicationController
 
     def index
-
+        prompts = Prompt.all
+        render json: prompts
     end
 
     def create
-
+        prompt = Prompt.new(prompt_params)
+        if prompt.save
+            render json: prompt, status: :accepted
+        else 
+            render json: {errors: prompt.errors.fullmesasges}, status: :unprocessable_entity
+        end
     end
+
+    private
+
+    def prompt_params
+        params.require(:prompt).permit(:content, :theme_id)
+    end
+
 end
